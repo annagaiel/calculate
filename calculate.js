d1fa0e7638de524feb048258a481e753801ed620
Anna Natorilla <annagaiel@gmail.com>
2016-05-31 08:45:00 -0700

    Initial Commit

---
+++ calculate.js
@@ -0,0 +1,27 @@
+var num1 = 4
+var num2 = 3
+
+var add = function(){
+  var result = num1 + num2;
+  alert(result);
+}
+
+var subtract = function(){
+  var result = num1 - num2;
+  alert(result);
+}
+
+var multiply = function(){
+  var result = num1 * num2;
+  alert(result);
+}
+
+var divide = function(){
+  var result = num1 - num2;
+  alert(result);
+}
+
+add();
+subtract();
+multiply();
+divide();
